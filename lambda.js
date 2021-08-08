e47a4ffb4315f3b4cf0ed183c72e8bd657d8cdc2
Sahil Purav <sahil.purav@cactusglobal.com>
2021-08-08 14:47:00 +0530

    Upgraded package dependancies and removed deprecated packages

--- lambda.js
+++ lambda.js
@@ -1,8 +1,5 @@
-const awsServerlessExpress = require("aws-serverless-express");
+const serverlessExpress = require("@vendia/serverless-express");
 const app = require("./dist/ngx-serverless-starter/serverless/main");
-const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 
-
-app.server.use(awsServerlessExpressMiddleware.eventContext());
-const serverProxy = awsServerlessExpress.createServer(app.server);
-module.exports.handler = (event, context) => awsServerlessExpress.proxy(serverProxy, event, context);
+const serverProxy = serverlessExpress.createServer(app.server);
+module.exports.handler = (event, context) => serverlessExpress.proxy(serverProxy, event, context);
